88b52b3d4e764b54f624513d30679e13b2e58876
Abel Csukas <csukasabel@gmail.com>
2026-08-18 15:18:57 +0200

    joke.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 from commands.hello import hello
 from commands.szabi import szabi
+from commands.joke import joke
 
 load_dotenv()
 
@@ -20,6 +21,7 @@ class MyClient(discord.Client):
     async def setup_hook(self):
         self.tree.add_command(hello)
         self.tree.add_command(szabi)
+        self.tree.add_command(joke)
         await self.tree.sync()
 
 
---
+++ commands/joke.py
@@ -0,0 +1,15 @@
+import requests
+import discord
+from discord import app_commands
+
+url = "https://official-joke-api.appspot.com/jokes/random"
+
+def getjoke():
+    data = (requests.get(url).json())
+    vicc = (data["setup"], data["punchline"])
+    return vicc 
+
+
+@app_commands.command(name="tellmeajoke", description="I'll tell you a hilarious joke")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"- {getjoke()[0]}\n- {getjoke()[1]}")
